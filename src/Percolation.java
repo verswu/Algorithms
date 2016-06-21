1a120743ad16194aaec03d902f155ecc56ff29c6
Kent Wu <haoyu.k.wu@gmail.com>
2016-06-21 03:05:32 -0700

    added algs4 jar

---
+++ src/Percolation.java
@@ -0,0 +1,30 @@
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Percolation {
+	
+	//creates N by N grid with all sites blocked
+	public Percolation(int N) {
+		
+	}
+	
+	//open site row i column j if not already open
+	public void open(int i, int j) {
+		
+	}
+	
+	//returns if row i column j is open?
+	public boolean isOpen(int i, int j) {
+		
+	}
+	
+	//returns if row i column j is full
+	public boolean isFull(int i, int j) {
+		
+	}
+	
+	//test client
+	public static void main(String[] args) {
+		
+	}
+
+}
